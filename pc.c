835494fa2a5dfd581bc7f3e621dd57619e78484b
nullifiedcat <nullifiedcat@gmail.com>
2017-09-20 08:34:49 +0300

    hello world

---
+++ pc.c
@@ -0,0 +1,10 @@
+#include "pc.h"
+
+#include <stdio.h>
+
+int
+main(int argc, const char **argv)
+{
+    printf("Hello world\n");
+    return 0;
+}
---
+++ pc.h
@@ -0,0 +1,7 @@
+#ifndef PC_H
+#define PC_H
+
+int
+main(int argc, const char **argv);
+
+#endif /* PC_H */
